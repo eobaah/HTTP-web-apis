c25ff08ddec556ee5a435e5d351ed365151138d4
eobaah <eobaah@gmail.com>
2017-04-27 17:43:54 -0700

    edit tweets wip

--- webserver/public/javascripts/main.js
+++ webserver/public/javascripts/main.js
@@ -1,30 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const tweetForm = document.querySelector('.createTweetForm')
 
-  tweetForm.addEventListener('submit', (event) => {
-    event.preventDefault()
-    const status = document.getElementById( 'msg' ).value
-    const media_url = document.getElementById( 'img' ).value
-    const body = JSON.stringify({ status, media_url })
-    console.log( "body:", body )
-    var init = {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      mode: 'cors',
-      cache: 'default',
-      body: body
-    }
-    fetch('/create', init)
-    .then( fetchContainer => {
-      // localStorage.setItem('log', fetchContainer)
-      return fetchContainer.text()
-    })
-    .then( redirectPath => {
-      // localStorage.setItem('log', redirectPath)
-      window.location.href = redirectPath
-    })
-    .catch( error => {
-      console.log(error)
-    })
+
+function deleteTweet(id_str){
+  console.log("WHAT?!?!?!", id_str)
+  var init = {
+    method: 'DELETE',
+    headers: {'Content-Type': 'application/json'},
+    mode: 'cors',
+    cache: 'default'
+  }
+  fetch('/delete/'+id_str, init)
+  .then( fetchContainer => {
+    // localStorage.setItem('log', fetchContainer)
+    return fetchContainer.text()
+  })
+  .then( redirectPath => {
+    // localStorage.setItem('log', redirectPath)
+    window.location.href = redirectPath
+  })
+  .catch( error => {
+    console.log(error)
+  })
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('#yesButton').addEventListener('click', function() {
+    deleteTweet(document.querySelector('h5').innerHTML)
   })
 })
